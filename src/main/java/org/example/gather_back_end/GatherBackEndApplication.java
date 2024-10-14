dd6518a6be56d1af3fa21bf35fcf61f9db006d23
Jindongleee <jindong1019@gmail.com>
2024-10-15 00:59:41 +0900

    🎉Start: Start New Project

---
+++ src/main/java/org/example/gather_back_end/GatherBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.gather_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatherBackEndApplication.class, args);
+    }
+
+}
