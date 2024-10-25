e91d1e4403f75bddb036743970e21fb7a037e8e5
Jindongleee <jindong1019@gmail.com>
2024-10-25 20:28:29 +0900

    Jwt Setting(#3)

--- src/main/java/org/example/gather_back_end/util/jwt/util/JWTUtil.java
+++ src/main/java/org/example/gather_back_end/util/jwt/util/JWTUtil.java
@@ -10,39 +10,31 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
-@Slf4j
 @Component
 public class JWTUtil {
-
     private final SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-
-
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
-
+    //지금 사용하고 있는 username을 받아온다.
     public String getUsername(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
     }
-
     public String getRole(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
-
     public Boolean isExpired(String token) {
-
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
     public String createJwt(String username, String role, Long expiredMs) {
-
         return Jwts.builder()
                 .claim("username", username)
                 .claim("role", role)
+                //token 생성
                 .issuedAt(new Date(System.currentTimeMillis()))
+                //token 만료시간
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
