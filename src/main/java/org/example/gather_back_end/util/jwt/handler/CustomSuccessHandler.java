5dee212980ea2064d40290426fa4cba7661ce957
Jindongleee <jindong1019@gmail.com>
2024-10-25 19:30:18 +0900

    Jwt Setting(#3)

--- src/main/java/org/example/gather_back_end/util/jwt/handler/CustomSuccessHandler.java
+++ src/main/java/org/example/gather_back_end/util/jwt/handler/CustomSuccessHandler.java
@@ -1,8 +1,10 @@
 package org.example.gather_back_end.util.jwt.handler;
 
-import jakarta.servlet.ServletException;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.gather_back_end.util.jwt.dto.CustomOAuth2User;
 import org.example.gather_back_end.util.jwt.util.JWTUtil;
 import org.springframework.security.core.Authentication;
@@ -14,18 +16,18 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JWTUtil jwtUtil;
 
-    public CustomSuccessHandler(JWTUtil jwtUtil) {
-
-        this.jwtUtil = jwtUtil;
-    }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
+        System.out.println("token is nulldawdawdadadawdawdawd");
 
         //OAuth2User
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
@@ -40,9 +42,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         String token = jwtUtil.createJwt(username, role, 60*60*1000L);
 
-        if(token.equals("null")){
-            System.out.println("token is null");
-        }
         response.sendRedirect("https://www.to-gather.info?code="+"Bearer "+token);
     }
 }
