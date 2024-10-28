734047ef3d0d3e551281819df30c59341aeb95bf
MinseoKangQ <kms02171@naver.com>
2024-10-28 23:58:50 +0900

    :sparkles: FeignClient 기능 활성화

--- src/main/java/org/example/gather_back_end/GatherBackEndApplication.java
+++ src/main/java/org/example/gather_back_end/GatherBackEndApplication.java
@@ -2,9 +2,11 @@ package org.example.gather_back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class GatherBackEndApplication {
 
