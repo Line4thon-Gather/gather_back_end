5ac031349bc993c5b1142c19b1a8877b6d087343
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-10-31 02:09:59 +0900

    🐛 버그 수정 (#15)

--- src/main/java/org/example/gather_back_end/util/jwt/entity/User.java
+++ src/main/java/org/example/gather_back_end/util/jwt/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,21 +13,52 @@ import org.example.gather_back_end.util.entity.BaseEntity;
 
 @Entity
 @Getter
-@Setter // TODO: 삭제
-@NoArgsConstructor(access = AccessLevel.PUBLIC) // TODO : PROTECTED로 변경
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // TODO : PROTECTED로 변경
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //provider Id
     private String username;
 
+    //실명
     private String name;
 
+    //이메일
     private String email;
 
+    //권한
     private String role;
 
+    //크리에이터명
     private String nickname;
+
+    @Builder
+    private User(Long id, String username, String name, String email, String role, String nickname) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.nickname = nickname;
+    }
+
+    public static User createAllUserInfo(String username, String name, String email, String role, String nickname) {
+        return User.builder()
+                .username(username)
+                .name(name)
+                .email(email)
+                .role(role)
+                .nickname(nickname)
+                .build();
+    }
+
+    // 업데이트 메서드
+    public void updateUserInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
 }
