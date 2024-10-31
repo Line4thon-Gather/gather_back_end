6c63c9ba8792283b76ff0ceaf5c147ed103f3d83
MinseoKangQ <kms02171@naver.com>
2024-10-31 23:39:01 +0900

    :memo: BaseResponse에 Swagger 적용

--- src/main/java/org/example/gather_back_end/util/response/BaseResponse.java
+++ src/main/java/org/example/gather_back_end/util/response/BaseResponse.java
@@ -1,5 +1,6 @@
 package org.example.gather_back_end.util.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.Getter;
@@ -12,9 +13,16 @@ import org.example.gather_back_end.util.exception.BaseErrorCode;
 @RequiredArgsConstructor
 public class BaseResponse {
 
+    @Schema(description = "API 호출 일시", example = "2024-10-30T15:38:12.43483271")
     private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+
+    @Schema(description = "API 호출 성공 여부", example = "true")
     private final Boolean isSuccess;
+
+    @Schema(description = "응답 코드", example = "200")
     private final String code;
+
+    @Schema(description = "응답 메시지", example = "호출에 성공하였습니다.")
     private final String message;
 
     public static BaseResponse of(Boolean isSuccess, BaseErrorCode errorCode) {
