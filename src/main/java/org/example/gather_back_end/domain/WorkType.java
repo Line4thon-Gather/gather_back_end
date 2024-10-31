4434677680e258752a5a8133c1d17e997a1d03e2
MinseoKangQ <kms02171@naver.com>
2024-10-31 16:35:08 +0900

    :card_file_box: WorkType에 기타 추가

--- src/main/java/org/example/gather_back_end/domain/WorkType.java
+++ src/main/java/org/example/gather_back_end/domain/WorkType.java
@@ -10,7 +10,8 @@ public enum WorkType {
 
     PRINTS("PRINTS"), // 인쇄물 (포스터, 배너 등)
     VIDEO("VIDEO"), // 영상 (숏폼, 미드폼, 롱폼 등)
-    SNS_POST("SNS_POST"); // SNS 게시물 (카드뉴스, 피드 등)
+    SNS_POST("SNS_POST"), // SNS 게시물 (카드뉴스, 피드 등)
+    ETC("ETC"); // 기타
 
     private String value;
 }
