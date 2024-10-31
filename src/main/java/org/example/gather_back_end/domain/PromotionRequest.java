d6122db32e88c7ed85c9d1b436bff6733eed1ab9
MinseoKangQ <kms02171@naver.com>
2024-10-31 15:55:42 +0900

    :card_file_box: 홍보 요청 내역 테이블 구현

---
+++ src/main/java/org/example/gather_back_end/domain/PromotionRequest.java
@@ -0,0 +1,59 @@
+package org.example.gather_back_end.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.gather_back_end.util.entity.BaseEntity;
+
+// 홍보 요청 내역
+@Entity
+@Table(name = "PromotionRequest")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PromotionRequest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "promotion_request_id")
+    private Long id;
+
+    // TODO: 회원 FK 연관관계 (일대다)
+
+    // 홍보 제목
+    @Column(nullable = false)
+    private String title;
+
+    // 예상 모집 기간
+    @Column(nullable = false)
+    private Integer period;
+
+    // 목표 인원
+    @Column(nullable = false)
+    private Integer targetNumberOfPeople;
+
+    // 보유한 예산
+    @Column(nullable = false)
+    private Long budget;
+
+    // 1순위 홍보 수단
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WorkType firstMeans;
+
+    // 2순위 홍보 수단
+    @Enumerated(EnumType.STRING)
+    private WorkType secondMeans;
+
+    // 3순위 홍보 수단
+    @Enumerated(EnumType.STRING)
+    private WorkType thirdMeans;
+}
