2380acd19b2d4928f730d26ff531d1117b30249e
Minseo Kang <kms02171@naver.com>
2024-10-31 23:50:03 +0900

    :memo: Swagger에 Tag 추가 (#22)

--- src/main/java/org/example/gather_back_end/openai/controller/OpenAiControllerApi.java
+++ src/main/java/org/example/gather_back_end/openai/controller/OpenAiControllerApi.java
@@ -1,12 +1,14 @@
 package org.example.gather_back_end.openai.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.gather_back_end.openai.dto.CustomOpenAiClientRequest;
 import org.example.gather_back_end.openai.dto.CustomOpenAiClientResponse;
 import org.example.gather_back_end.util.response.SuccessResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Tag(name = "Open AI 관련", description = "마케팅 전략 생성에 사용")
 public interface OpenAiControllerApi {
 
     @Operation(summary = "Open AI 테스트용")
