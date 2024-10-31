cf56af7c88ea7a56cfcda03f201465527d003eec
MinseoKangQ <kms02171@naver.com>
2024-10-31 16:51:09 +0900

    :card_file_box: 회원 테이블 추가 필드 작성

--- src/main/java/org/example/gather_back_end/util/jwt/entity/User.java
+++ src/main/java/org/example/gather_back_end/util/jwt/entity/User.java
@@ -1,39 +1,61 @@
 package org.example.gather_back_end.util.jwt.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.example.gather_back_end.util.entity.BaseEntity;
 
 @Entity
+@Table(name = "Users")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED) // TODO : PROTECTED로 변경
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //provider Id
+    // provider Id
     private String username;
 
-    //실명
+    // 실명
     private String name;
 
-    //이메일
+    // 이메일
     private String email;
 
-    //권한
+    // 권한
     private String role;
 
-    //크리에이터명
+    // 크리에이터명
     private String nickname;
+
+    // 크리에이터 프로필 사진 주소
+    @Column(columnDefinition = "TEXT")
+    private String profileImgUrl;
+
+    // 소개글 제목
+    @Column(length = 20)
+    private String introductionTitle;
+
+    // 소개글 내용
+    @Column(length = 50)
+    private String introductionContent;
+
+    // 연락 문의 카카오톡 아이디
+    @Column(unique = true)
+    private String contactKakaoId;
+
+    // 연락 문의 이메일 주소
+    @Column(unique = true)
+    private String contactEmail;
 
     @Builder
     private User(Long id, String username, String name, String email, String role, String nickname) {
