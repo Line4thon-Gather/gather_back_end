a4534a32bae2b7f5338e46bb6a096e22b2ad41d4
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-11-03 00:23:21 +0900

    🐛 버그 수정 (#27)

--- src/main/java/org/example/gather_back_end/openai/controller/OpenAiController.java
+++ src/main/java/org/example/gather_back_end/openai/controller/OpenAiController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.gather_back_end.openai.dto.CustomOpenAiClientRequest;
 import org.example.gather_back_end.openai.dto.CustomOpenAiClientResponse;
 import org.example.gather_back_end.openai.service.OpenAiService;
+import org.example.gather_back_end.util.jwt.dto.CustomOAuth2User;
 import org.example.gather_back_end.util.response.SuccessResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,12 @@ public class OpenAiController implements OpenAiControllerApi {
     private final OpenAiService openAiService;
 
     @PostMapping("/test")
-    public SuccessResponse<CustomOpenAiClientResponse> requestToOpenAi(@RequestBody CustomOpenAiClientRequest request) {
+    public SuccessResponse<CustomOpenAiClientResponse> requestToOpenAi(Authentication authentication,@RequestBody CustomOpenAiClientRequest request) {
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        if(customOAuth2User == null) {
+            return null;
+        }
+
         CustomOpenAiClientResponse response = openAiService.getOpenAiResponse(request);
         return SuccessResponse.of(response);
     }
