93980e5fbe0c6c8c5fcb658780056974c4efab8e
MinseoKangQ <kms02171@naver.com>
2024-11-04 01:11:57 +0900

    :memo: Swagger 문서 업데이트

--- src/main/java/org/example/gather_back_end/certification/dto/CertificationEntrepreneurValidateReq.java
+++ src/main/java/org/example/gather_back_end/certification/dto/CertificationEntrepreneurValidateReq.java
@@ -1,8 +1,13 @@
 package org.example.gather_back_end.certification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CertificationEntrepreneurValidateReq(
+        @Schema(description = "사업자 등록번호", example = "1496200716")
         String b_no,
+        @Schema(description = "개업일자", example = "20240603")
         String start_dt,
+        @Schema(description = "대표자 성명", example = "최용석")
         String p_nm
 ) {
 }
--- src/main/java/org/example/gather_back_end/certification/dto/CertificationEntrepreneurValidateRes.java
+++ src/main/java/org/example/gather_back_end/certification/dto/CertificationEntrepreneurValidateRes.java
@@ -1,10 +1,13 @@
 package org.example.gather_back_end.certification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record CertificationEntrepreneurValidateRes(
+        @Schema(description = "사업자 인증 성공 여부", example = "true")
         boolean isSuccess,
+        @Schema(description = "사업자 인증 결과 메시지", example = "사업자 등록 인증 성공")
         String message
 ) {
 
