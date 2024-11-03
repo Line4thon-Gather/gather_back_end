b7ad198fe2aa456bfae6b7adcf3a561d667565fe
MinseoKangQ <kms02171@naver.com>
2024-11-03 15:48:58 +0900

    :pencil2: 주석 제거

--- src/main/java/org/example/gather_back_end/certification/service/CertificationServiceImpl.java
+++ src/main/java/org/example/gather_back_end/certification/service/CertificationServiceImpl.java
@@ -50,15 +50,9 @@ public class CertificationServiceImpl implements CertificationService {
 
     @Override
     public CertificationEntrepreneurRes certificationEntrepreneur(CertificationEntrepreneurReq req) {
-        // EntrepreneurClientReq 객체 생성 (사업자 번호를 리스트 형태로 전달)
         EntrepreneurClientReq clientReq = new EntrepreneurClientReq(List.of(req.number()));
-
-        // Feign 클라이언트를 통해 인증 요청
         EntrepreneurClientRes clientRes = entrepreneurClient.getEntrepreneurClientAuthInfo(businessApiKey, clientReq);
-
-        // 응답 상태 확인: status_code가 "OK"이고 match_cnt가 0보다 큰 경우 성공으로 간주
         boolean isSuccess = clientRes != null && "OK".equals(clientRes.status_code()) && clientRes.match_cnt() > 0;
-
         return CertificationEntrepreneurRes.from(isSuccess);
     }
 
