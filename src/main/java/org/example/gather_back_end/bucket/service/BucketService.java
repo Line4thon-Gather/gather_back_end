f2ebbe64d072a9cc9f6fbf2e38ce3784651b73ee
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-11-07 01:33:40 +0900

    refactor/#54 (#60)
    * ♻️ 크리에이터 등록 수정
    
    * ♻️ 크리에이터 등록 수정 (기본 이미지 포함)

--- src/main/java/org/example/gather_back_end/bucket/service/BucketService.java
+++ src/main/java/org/example/gather_back_end/bucket/service/BucketService.java
@@ -6,11 +6,13 @@ import org.springframework.web.multipart.MultipartFile;
 public interface BucketService {
 
     // 프로필 이미지 업로드
-    UploadProfileImgRes uploadProfileImg(MultipartFile file, Long userId) throws Exception;
+    void uploadProfileImg(MultipartFile file, Long userId) throws Exception;
 
     // 파일 경로 받아오기
     Long getPublicImgUrl(Long userId) throws Exception;
 
     // 파일 다운로드
     MultipartFile downloadFile(Long userId, String type) throws Exception;
+
+    String defaultProfileImgUrl();
 }
