470820a5bacd01bce09080a9083f4045436a3778
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-11-07 22:22:52 +0900

    ✨ 포트폴리오 PDF Bucket + 썸네일 IMG Bucket (#69)

--- src/main/java/org/example/gather_back_end/portfolio/dto/CreatePortfolioReq.java
+++ src/main/java/org/example/gather_back_end/portfolio/dto/CreatePortfolioReq.java
@@ -1,11 +1,12 @@
 package org.example.gather_back_end.portfolio.dto;
 
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record CreatePortfolioReq(
         String title,
-        String thumbnailImgUrl,
-        String fileUrl
+        MultipartFile thumbnailImgUrl,
+        MultipartFile fileUrl
 ) {
 }
