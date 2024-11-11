640879dc57a9aaf20f9248a07d0e0720e69920bc
MinseoKangQ <kms02171@naver.com>
2024-11-12 01:55:16 +0900

    :art: 컨트롤러에서 사용할 dto 정의

---
+++ src/main/java/org/example/gather_back_end/promotion/dto/cost/PromotionCostReq.java
@@ -0,0 +1,21 @@
+package org.example.gather_back_end.promotion.dto.cost;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PromotionCostReq(
+        @Schema(description = "사용자가 입력한 예산", example = "646000")
+        Integer budget,
+
+        @Schema(description = "1순위 홍보 수단 (필수)", example = "PRINTS")
+        String firstMeans,
+
+        @Schema(description = "2순위 홍보 수단 (null 가능)", example = "VIDEO")
+        String secondMeans,
+
+        @Schema(description = "3순위 홍보 수단  (null 가능)", example = "SNS_POST")
+        String thirdMeans,
+
+        @Schema(description = "인스타그램 홍보 기간", example = "20")
+        Integer instagramPromotionPeriod
+) {
+}
