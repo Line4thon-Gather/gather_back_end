5ee6cb667eb0dcb985c9649b54e48242984437fe
MinseoKangQ <kms02171@naver.com>
2024-11-13 15:03:37 +0900

    :memo: Swagger 문서 업데이트

--- src/main/java/org/example/gather_back_end/promotion/controller/PromotionController.java
+++ src/main/java/org/example/gather_back_end/promotion/controller/PromotionController.java
@@ -1,13 +1,9 @@
 package org.example.gather_back_end.promotion.controller;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.gather_back_end.promotion.dto.cost.PromotionCostReq;
-import org.example.gather_back_end.promotion.dto.cost.PromotionCostRes;
 import org.example.gather_back_end.promotion.dto.promotion.PromotionRes;
 import org.example.gather_back_end.promotion.dto.timeline.PromotionTimelineReq;
-import org.example.gather_back_end.promotion.dto.timeline.PromotionTimelineRes;
 import org.example.gather_back_end.promotion.service.PromotionService;
 import org.example.gather_back_end.util.jwt.dto.CustomOAuth2User;
 import org.example.gather_back_end.util.response.SuccessResponse;
