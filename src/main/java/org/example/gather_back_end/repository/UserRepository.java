723a5b88350faf9a026647c5d03d2eb5d619ce87
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-11-15 22:55:58 +0900

    ✨ 마이페이지 구성하기 (#114)

--- src/main/java/org/example/gather_back_end/repository/UserRepository.java
+++ src/main/java/org/example/gather_back_end/repository/UserRepository.java
@@ -63,4 +63,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
            "AND (:category IS NULL OR w.category = :category)")
     Page<User> customFiltering(@Param("price") Integer price, @Param("category") WorkType category, Pageable pageable);
 
+    @Query("SELECT u FROM User u " +
+            "JOIN u.workList w " +
+            "JOIN u.portfolioList p " +
+            "WHERE u.introductionTitle IS NOT NULL " + // 소개글 제목 존재
+            "AND SIZE(u.workList) > 0 " + // 작업 가능 항목 등록
+            "AND SIZE(u.portfolioList) > 0 ")
+    User findByMyPage(User user);  // 마이페이지 유저 찾기
 }
