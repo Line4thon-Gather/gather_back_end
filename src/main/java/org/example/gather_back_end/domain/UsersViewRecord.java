cc5b2223593f4c2f1591896bdde580e8c632f83c
Eddy <149242983+Jindongleee@users.noreply.github.com>
2024-11-15 23:06:36 +0900

    ✨ 내가 조회한 크리에이터 기능 추가 (#115)
    * :bug: 오류 해결
    
    * :card_file_box: ViewRecord 테이블 추가
    
    * :card_file_box: UsersViewRecord 테이블 추가
    
    * :card_file_box: 연관관계 설정
    
    * :bug: 컬럼명 변경
    
    * :card_file_box: BaseEntity 추가
    
    * :sparkles: ViewRecordRepository와 Exception 작성
    
    * :sparkles: UsersViewRecordRepository와 Exception 작성
    
    * :art: ViewService 인터페이스 작성
    
    ---------
    
    Co-authored-by: MinseoKangQ <kms02171@naver.com>

---
+++ src/main/java/org/example/gather_back_end/domain/UsersViewRecord.java
@@ -0,0 +1,40 @@
+package org.example.gather_back_end.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.gather_back_end.util.entity.BaseEntity;
+
+@Entity
+@Table(name = "ViewRecord")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UsersViewRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 회원 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 기록 테이블 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "view_record_id")
+    private ViewRecord viewRecord;
+
+}
